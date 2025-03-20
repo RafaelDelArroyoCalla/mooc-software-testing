3c78fcaa8f6e0e77d44ffd1d80558f0e995cfbab
Rafael Del Arroyo <rafael.delarroyo@ucsm.edu.pe>
2025-03-19 22:50:41 -0500

    fix: corregir estructura de la condicional en el metodo find dentro del archivo NumFinder.java

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -3,13 +3,12 @@ package tudelft.numfinder;
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
-
+    //Error en la estructura de la condicional
     public void find(int[] nums) {
         for(int n : nums) {
-
             if(n < smallest)
                 smallest = n;
-            else if (n > largest)
+            if (n > largest)
                 largest = n;
 
         }
