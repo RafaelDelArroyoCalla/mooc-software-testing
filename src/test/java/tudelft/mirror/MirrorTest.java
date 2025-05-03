7846a3a3abb3283a1df604c331cc5630827ee94f
RafaelDelArroyoCalla <dacrafaelraul@gmail.com>
2025-05-02 21:07:12 -0500

    test: agregar casos de prueba para el método mirrorEnds y validar su funcionalidad

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,19 @@
 package tudelft.mirror;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void testMirrorEnds() {
+        Mirror mirror = new Mirror();
+
+        // Casos de prueba
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+        assertEquals("a", mirror.mirrorEnds("abca"));
+        assertEquals("", mirror.mirrorEnds("abcde"));
+        assertEquals("x", mirror.mirrorEnds("x"));
+        assertEquals("", mirror.mirrorEnds("xyz"));
+    }
 }
